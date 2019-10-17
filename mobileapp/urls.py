bedfec369a576fee24da728ded702fe7f349cc28
Dusembekd <dusembekdd@gmail.com>
2019-10-18 02:40:17 +0600

    registration page has created

--- mobileapp/urls.py
+++ mobileapp/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, auth
 from django.urls import path
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #AUTH
+    path('signup', views.SignUp.as_view(), name = 'signup'),
+    path('login', auth.LogIn.as_view(), name='login'),
+    path('logout', auth.LogOut.as_view(), name='logout'),
+
 ]
